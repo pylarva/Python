a1653c8a0b08aefe10f03d288695036fd8f015f6
pylarva <1326126359@qq.com>
2016-09-17 20:40:34 +0800

    0917

--- day1/info_1.py
+++ day1/info_1.py
@@ -2,3 +2,14 @@
 # -*- coding:utf-8 -*-
 # Author:lichengbing
 
+import getpass
+
+#name = input("name:")
+#passwd = getpass.getpass("passwd:")
+
+#print(name,passwd)
+
+username = input("uesrname:")
+password = getpass.getpass('password: ')
+print(username,password)
+
