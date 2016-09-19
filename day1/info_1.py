c1edb812f51f3683b9048878c22b2c80b9f8b763
pylarva <1326126359@qq.com>
2016-09-19 23:54:57 +0800

    0919

--- day1/info_1.py
+++ day1/info_1.py
@@ -9,7 +9,36 @@ import getpass
 
 #print(name,passwd)
 
-username = input("uesrname:")
-password = getpass.getpass('password: ')
-print(username,password)
+#username = input("uesrname:")
+#password = getpass.getpass('password: ')
+#print(username,password)
 
+#for i,j in enumerate(('a','b','c')):
+#    print(i,j)
+'''
+def show(b='',c=''):
+    print(b,c)
+
+a=1
+b=2
+show(a,b)
+'''
+a = {
+    1:[1,2,3],
+    2:{
+        21:{
+            'a',
+            'b',
+        },
+        22:[4,5,6]
+    },
+    3:{
+        31:[7,8,9]
+    }
+}
+print(type(a))
+print(type(a[2]))
+print(type(a[3][31]))
+
+b = a[2]
+print(b)
