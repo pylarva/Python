5780fd7e3b0cb829c78e11f3e82fa813d3e0c772
pylarva <1326126359@qq.com>
2016-09-23 15:14:04 +0800

    0923

--- day1/info_1.py
+++ day1/info_1.py
@@ -43,14 +43,29 @@ a = {
 haha_dict = {}
 for i,j in enumerate(a[2][21]):
     haha_dict[i] = j
-    print('%d.%s' % (i, j))
+#    print('%d.%s' % (i, j))
 #print(haha_dict)
 b = a[2][21]
 #print(b)
 #print('%d.%s'% (i,j))
 
 c = {
-    100:['1','2','3']
+    100:[('1',11),('2',22),('3',33)],
+    101:[('A',11),('B',22),('C',33)]
 }
-print(type(c[100]))
-print(c[100][0])
+#print(type(c[100]))
+#print(c[100][0])
+#print(c.keys())
+
+d = []
+d.append(('a',2,1888))
+
+#print(d)
+#print(d[0][2])
+
+f = open('C:/Users/lilongzi/Desktop/test.txt')
+a_line = f.read()
+b_line = f.readline()
+c_line = f.readlines()
+
+print('1:',b_line)
