24ffdfb01d898fda2fd8aefefb07474e0e27ca99
pylarva <1326126359@qq.com>
2016-10-19 23:19:47 +0800

    1019

---
+++ day4/s2.py
@@ -0,0 +1,20 @@
+#！/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:lichengbing
+
+
+def outer(func):
+    def inner():
+        print('log')
+        return func()
+    return inner
+
+
+@outer
+def f1():
+    print('F1')
+
+
+@outer
+def f2():
+    print('F2')
