1085a6de7c04852a267dffd7ed2d978499c1871e
pylarva <1326126359@qq.com>
2016-10-30 14:50:38 +0800

    1030

--- day5/time.py
+++ day5/time.py
@@ -11,19 +11,35 @@ import time
 # print(time.localtime())
 # print(time.mktime(time_obj))  # 转时间戳
 #
-# # 将struct_time格式转换成指定格式
+# 将struct_time格式转换成指定格式
+# print(time.localtime())
 # print(time.strftime("%Y-%m-%d %H:%S", time.localtime()))
 #
-# # 与strftime相反
+# # 与strftime相反 将字符串转换成strcut格式
 # tm = time.strptime("2016-10-10 10:50", "%Y-%m-%d %H:%M")
 # print(tm)
+
+# print(time.time())
+# print(time.strftime('%Y-%m-%d'))
+# print(time.localtime())
+# print(time.ctime())
+# print(time.ctime(time.time()-86400))
+#
+# print(time.gmtime())
+# time_obj = time.gmtime()
+# print(time_obj.tm_year, time_obj.tm_mon, time_obj.tm_mday)
+# print(time.mktime(time_obj))
+
+
+
+
 
 
 import datetime
 
+print(datetime.datetime.now())  # 显示当前时间
 print(datetime.date.today())
 print(datetime.date.fromtimestamp(time.time()-86400))  # 将时间戳转换成格式化日期
-print(datetime.datetime.now())  # 显示当前时间
 
 # 时间加减
 print(datetime.datetime.now() + datetime.timedelta(days=10))  # 比现在加10天
