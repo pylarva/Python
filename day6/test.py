567d223a01052ca818d4e07724dcb8b1f9b55b47
pylarva <1326126359@qq.com>
2016-11-04 13:24:55 +0800

    1104

--- day6/test.py
+++ day6/test.py
@@ -4,13 +4,37 @@
 # Author:lichengbing
 import re
 
-s = '8+2'
-s1 = '-8'
+s = '2*(-8)-9'
 
-# t = re.split('(\d+[*/]\d+)', s, 1)
-# print(t)
+# 取第一个括号内容
+s1 = re.search('\(([0-9 *+-/]+)\)', s).groups()
+print(s1)
 
-t = re.search('(.*)', s).groups()
+# 处理乘除
+s1 = s1[0]
+# if s1
+
+s_before = re.split('\(-*\d+[*/]\d+\)', s, 1)[0]
+s_back = re.split('\(-*\d+[*/]\d+\)', s, 1)[1]
+print(s_before, s_back)
+
+
+f = re.search('-*\d+[*/]\d+', s1).group()  # 乘表达式
+print(f)
+t1 = re.split('[*]', f)[0]
+t2 = re.split('[*]', f)[1]
+print(t1, t2)
+t = int(t1) * int(t2)  # 乘的结果
 print(t)
+t = str(t)
+if int(t) < 0:
+    s = s_before + '(' + t + ')' + s_back
+else:
+    s = s_before + t + s_back
+print(s)
 
-print(int(s1))
+# 处理加减
+# s1 = s1[0]
+# f = re.search('-*\d+[+-]\d+', s1).group()  # 加减表达式
+# print(f)
+
