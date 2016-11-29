ef726577d76bd94368441fdd77b1d7ef3ce3e7f9
pylarva <1326126359@qq.com>
2016-11-29 23:15:35 +0800

    1129

--- day9/socket/soctet_server.py
+++ day9/socket/soctet_server.py
@@ -6,35 +6,32 @@
 import socket
 import subprocess
 
+# 以元组形式定义一个IP地址个一个端口号
 ip_port = ('127.0.0.1', 9999)
 
-# 买手机
+# 创建socket对象
 s = socket.socket()
-
-# 买手机卡
+# 绑定上面创建的IP地址和端口
 s.bind(ip_port)
-
-# 开机
+# 开始监听
 s.listen(5)
 
-# 等待电话
+# 与客户端建立连接并开始会话
 while True:
-    conn, addr = s.accept()  # conn 客户端与服务端连接的专有线路
+    conn, addr = s.accept()  # conn 表示客户端与服务端连接的专有线路
 
-    # 收消息
     while True:
         try:
-            recv_data = conn.recv(1024)
+            recv_data = conn.recv(1024)  # 接受客户端发来的消息
             if str(recv_data, encoding='utf-8') == 'exit':
                 break
-            # 发消息
             p = subprocess.Popen(str(recv_data, encoding='utf-8'), shell=True, stdout=subprocess.PIPE)
             ret = p.stdout.read()
 
             if len(ret) == 0:
                 send_data = 'cmd error...'
                 ret = bytes(send_data, encoding='utf-8')
-                conn.send(ret)
+                conn.send(ret)  # 发送消息
             else:
                 ret = str(ret, encoding='gbk')
                 ret = bytes(ret, encoding='utf8')
@@ -47,5 +44,4 @@ while True:
         except Exception:
             break
 
-    # 挂电话
     conn.close()
