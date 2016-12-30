b87b42a9c49ea6df557c9c70eef4a98d29802ed0
pylarva <1326126359@qq.com>
2016-12-30 15:07:49 +0800

    1230

--- day12/code_rpc/server.py
+++ day12/code_rpc/server.py
@@ -5,6 +5,8 @@
 
 import re
 import pika
+import random
+import threading
 
 
 def publish_message(cmd, host_list):
@@ -12,13 +14,35 @@ def publish_message(cmd, host_list):
     channel = connection.channel()
     channel.exchange_declare(exchange='server',
                              type='fanout')
-    severity = host_list
 
-    channel.basic_publish(exchange='server', routing_key='', body=cmd)
-    print(" [x] Sent %r" % cmd)
+    task_id = random.randrange(1000, 9999)
+    message = '{} {}'.format(str(task_id), cmd)
+
+    channel.basic_publish(exchange='server', routing_key='', body=message)
+
+    print(" [x] Sent %r" % message)
+    print(" [x] Task ID: %s" % task_id)
+    return task_id
+
+
+def callback(ch, method, properties, body):
+    print(body)
+
+
+def receive_message(arg):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='10.0.0.111'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=arg)
+
+    channel.basic_consume(callback,
+                          queue=arg,
+                          no_ack=True)
+    channel.start_consuming()
 
 print('   \033[32;0m>> 主机管理 <<\033[0m   '.center(60, '-'))
 print('\033[32;0m命令示例：\033[0m \033[31;0m run "df -h" --hosts 192.168.3.55 10.4.3.4\033[0m')
+
 
 host_list = []
 while True:
@@ -27,6 +51,9 @@ while True:
     if not re.match('run \".*\" --hosts (25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}', inp):
         print('cmd error...')
     else:
-        host_list = inp.split(' ')[3:]
-        publish_message(inp, host_list)
+        host_list = inp.split('hosts ')[-1].split(' ')
+        task_id = publish_message(inp, host_list)
+        t = threading.Thread(target=receive_message, args=(task_id, ))
+        t.start()
+
 
---
+++ test/test.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:pylarva
+# bolg:www.lichengbing.com
+
+import random
+
+# s = 'run \"df -h\" --hosts 192.168.3.55 10.0.0.1'
+# li = ['10.0.0.1']
+#
+# ss = re.search('\".*\"', s).group()[1:-1]
+#
+# print(ss)
+print(random.randrange(1000, 9999))
+
+
+
+
+
+
