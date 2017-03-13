602f0bb74daadff81b0291ffe2f2f73ff2fa7986
pylarva <1326126359@qq.com>
2017-03-13 17:26:18 +0800

    obj19 add02

--- Projects/obj19/app01/views.py
+++ Projects/obj19/app01/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, HttpResponse
 from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    # 获取当前用户的随机字符串 根据该字符串查找对应信息
+    if request.session.get('is_login', None):
+        return render(request, 'index.html', {'username': request.session['username']})
+    else:
+        return HttpResponse('no')
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        u = request.POST.get('username')
+        p = request.POST.get('pwd')
+        print(u, p)
+        if u == "root" and p == '123':
+
+            # 设置session
+            request.session['username'] = u
+            request.session['is_login'] = True
+
+            return redirect('/index/')
+        else:
+            return render(request, 'login.html')
+    # return render(request, 'login.html')
+
+
+def logout(request):
+    request.session.clear()
+    return redirect('/login/')
