7c954416843849e2cabcaa978fe331cb30841d38
pylarva <lichengbing9027@gamil.com>
2017-09-07 09:33:49 +0800

    0907

--- Projects/xxdCmdb/scr/autopublishing.py
+++ Projects/xxdCmdb/scr/autopublishing.py
@@ -33,8 +33,8 @@ from threading import Timer
 
 API_HOST = 'cmdb.xxd.com'
 # API_URL = 'http://192.168.33.110:8005/api/release'
-API_URL = 'http://172.16.19.12:8005/api/release'
-# API_URL = 'http://cmdb.xinxindai.com/api/release'
+# API_URL = 'http://172.16.19.12:8005/api/release'
+API_URL = 'http://cmdb.xinxindai.com/api/release'
 TMP_DIR = '/tmp'
 LOGGER_FILE = '/home/admin/logs/autopublishing.log'
 RUNNING_USER = 'admin'
